e77703713bc826bd03d480abf11e71f16bb88ba5
The-Real-Dr-Joss <jossufigueiras@icloud.com>
2025-03-14 23:36:32 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    require_once("./include/config.php"); 
+    $titre = SITE_NAME . ' - Accueil';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo htmlspecialchars($titre);?></title>
+</head>
+<body>
+    <h1>VideVendu</h1>
+</body>
+</html>
