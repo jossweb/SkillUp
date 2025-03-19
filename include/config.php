77507b828169d67056bacfc8beb33047c67a5e0e
The-Real-Dr-Joss <jossufigueiras@icloud.com>
2025-03-19 22:53:32 +0100

    add starter colors in css and font

---
+++ include/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// Configuration globale du site
+define('SITE_NAME', 'VideVendu');
+
+// Configuration des chemins
+define('ASSETS_PATH', 'assets');
+define('CSS_PATH', ASSETS_PATH . '/css');
+define('JS_PATH', ASSETS_PATH . '/js');
+?>
