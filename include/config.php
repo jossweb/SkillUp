d3a17a7fb3d842d3fc2185659481a4ad97f996f0
The-Real-Dr-Joss <jossufigueiras@icloud.com>
2025-03-22 16:53:39 +0100

    change DB PASS for xamp and online server

--- include/config.php
+++ include/config.php
@@ -12,7 +12,7 @@ define('JS_PATH', ASSETS_PATH . '/js');
 define('DB_HOST', 'localhost'); // Hôte de la base de données
 define('DB_PORT', 3306);        // Port (3306 par défaut pour MySQL)
 define('DB_USER', 'root');      // Nom d'utilisateur
-define('DB_PASS', 'root');          // Mot de passe 'mamp = root' autres = '' (pas de mdp)
+define('DB_PASS', '');          // Mot de passe 'mamp = root' autres = '' (pas de mdp)
 define('DB_NAME', 'skillup');  // Nom de la base de données
 
 $dsn = "mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME . ";charset=utf8mb4"; //création de la string de connection à la db
