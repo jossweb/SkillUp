a11448dfa2f85b7b11a470efdfe6544760953d08
Keita <tenin.keita0004@gmail.com>
2025-03-22 23:38:01 +0100

    Ajout des fichiers de la page catégories

---
+++ catégorie.php
@@ -0,0 +1,14 @@
+<?php
+//Déclaration du tab $categories
+$categories = [
+    ["image" => "ImageCat/école.jpg", "titre" => "Éducation de base", "description" => "Les bases du savoir, la clé de votre réussite ! <br>Apprenez et progressez avec nous "],
+    ["image" => "ImageCat/informatic.jpg", "titre" => "Informatique", "description" => "Codez, créez, innovez ! <br>Dominez le monde digital avec nos formation en informatique"],
+    ["image" => "ImageCat/langue.jpg", "titre" => "Langues & communication", "description" => "Ouvrez les portes du monde en maîtrisant de nouvelles <br>langues avec nos formations"],
+    ["image" => "ImageCat/Busness.jpg", "titre" => "Gestions & affaires", "description" => "Maîtrisez l’art du business avec nos formations adaptés à tous <br>les niveaux"],
+    ["image" => "ImageCat/musique.jpg", "titre" => "Musique", "description" => "Composez votre avenir avec nos <br>formations de musique accessibles à tous"],
+    ["image" => "ImageCat/Art.jpg", "titre" => "Art & design", "description" => "Transformez votre imagination en <br>chef-d'œuvre avec nos formations en art et design"],
+    ["image" => "ImageCat/sante.jpg", "titre" => "Sciences médicales", "description" => "Prenez soin de votre savoir <br>avec nos formations en médecine et soins infirmiers"],
+    ["image" => "ImageCat/Social.jpg", "titre" => "Sciences sociales", "description" => "Décryptez la société avec <br>nos formations en sciences sociale et politique"],
+    ["image" => "ImageCat/ingénieur.jpg", "titre" => "Sciences & Ingénierie", "description" => "Imaginez, concevez, innovez ! <br>Devenez l’ingénieur du futur avec nos formations"]
+];   
+?>
