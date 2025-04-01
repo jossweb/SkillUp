621b104f7fd4438553135870165420e9133597db
The-Real-Dr-Joss <jossufigueiras@icloud.com>
2025-04-01 21:17:38 +0200

    delete ip

--- include/config.php
+++ include/config.php
@@ -12,7 +12,7 @@ define('JS_PATH', ASSETS_PATH . '/js');
 define('DB_HOST', 'localhost'); // Hôte de la base de données
 define('DB_PORT', 3306);        // Port (3306 par défaut pour MySQL)
 define('DB_USER', 'root');      // Nom d'utilisateur
-define('DB_PASS', 'root');          // Mot de passe 'mamp = root' autres = '' (pas de mdp)
+define('DB_PASS', '');          // Mot de passe 'mamp = root' autres = '' (pas de mdp)
 define('DB_NAME', 'skillup');  // Nom de la base de données
 
 define('DEBUG', true); //Si true alors les erreurs s'afficheront sur les pages 
