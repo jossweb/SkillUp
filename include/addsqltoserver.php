c1c2df5ec163648e9865e9485bbd3604ce70689c
The-Real-Dr-Joss <jossufigueiras@icloud.com>
2025-04-01 22:39:58 +0200

    add profil page (only front at this point)

--- include/addsqltoserver.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once("connectdb.php");
-    $db = connectDB();
-    $sql = "CREATE TABLE sessions ( id SERIAL PRIMARY KEY, user_id INTEGER NOT NULL, token VARCHAR(255) UNIQUE NOT NULL, ip_address VARCHAR(45) NOT NULL, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, expires_at TIMESTAMP NOT NULL, FOREIGN KEY (user_id) REFERENCES Utilisateurs(id) ON DELETE CASCADE );";
-    $stmt = $db->prepare($sql);
-    $stmt->execute();
-    $result = $stmt->fetch(PDO::FETCH_ASSOC); 
-    echo $result;
-?>
--- pages/profile.php
+++ pages/profile.php
@@ -2,13 +2,21 @@
 session_start();
 require_once("../include/connectdb.php"); 
 require_once("../include/sessionManager.php");
-if(!IsConnected($_SERVER['REMOTE_ADDR'])){
+if(!IsConnected()){
     header('Location:  connection.php');
     exit();
 }
-
 $titre = SITE_NAME . ' - profil';
 $db =  connectDB();
+$sql = 'SELECT Utilisateurs.prenom, Utilisateurs.nom, Utilisateurs.e_mail, Utilisateurs.avatar_url, Utilisateurs.role FROM sessions INNER JOIN Utilisateurs ON Utilisateurs.id = sessions.user_id WHERE sessions.token = :token';
+$request = $db->prepare($sql);
+$request->bindParam(':token', $_COOKIE['user_token']);
+$request->execute();
+$result = $request->fetch(PDO::FETCH_ASSOC);
+$avatar = $result['avatar_url'];
+if($avatar == null){
+    $avatar = 'https://remyweb.fr/images/1356835268082008064.webp';
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,20 +27,25 @@ $db =  connectDB();
     <title><?php echo $titre; ?></title>
 </head>
 <body>
-    <section>
-        <h1>Mon compte</h1>
-        <div>
+    <section class="infos">
+        <div class="user-info">
             <div class="headband">
-
+                <img src="<?php echo $avatar ?>" alt="Your profile picture" class="avatar"/>
             </div>
-            <h2>Prénom</h2>
-            <p>emaildetest@test.com</p>
+
+            <h2><?php echo $result['prenom']?></h2>
+            <p><?php echo $result['e_mail']?></p>
             <button>Changer mon mot de passe</button>
         </div>
-        <div>
+        <div class="change-info">
             <h2>Mes infos</h2>
+            <form method="POST">
+                <input type="test" placeholder="<?php echo $result['nom']?>">
+                <input type="test" placeholder="<?php echo $result['prenom']?>">
+                <button type="submit">Enregistrer</button>
+            </form>
         </div>
-        <div>
+        <div class="delete-account">
             <h2>Zone de danger</h2>
             <p>Cette action est permanente et ne pourra pas être annulée.</p>
             <button>Supprimer mon compte</button>
